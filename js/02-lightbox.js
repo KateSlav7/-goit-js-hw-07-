7b28699d730a805c92d27c4c5e46a70ab2344899
KateSlav7 <kateslavgorodskaya@gmail.com>
2021-11-16 13:54:06 +0200

    total_commits

---
+++ js/02-lightbox.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryDivEl = document.querySelector(".gallery");
+const cardsMarkup = createCardsMarkup(galleryItems);
+console.log(galleryDivEl);
+
+galleryDivEl.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createCardsMarkup(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <li>
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </li>
+    </div>    
+    `;
+    })
+    .join("");
+}
+galleryDivEl.addEventListener("click", onSelectImage);
+
+function onSelectImage(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const imgUrl = evt.target.dataset.source;
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
